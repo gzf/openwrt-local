f20b63d090e545f5e96993608d0f15db28b521a8
Zhifeng Gu <guzhifeng1979@hotmail.com>
2021-02-05 19:49:04 +0800

    create local image builder of 19.07

---
+++ xiaomi_R3P.sh
@@ -0,0 +1,4 @@
+make image PROFILE=xiaomi_mir3p \
+    PACKAGES="-odhcpd-ipv6only -iptables -ip6tables -firewall -kmod-ipt-offload -ppp -ppp-mod-pppoe \
+              vim \
+              kmod-usb-storage-uas kmod-fs-autofs4 kmod-fs-ext4"
---
+++ xiaoyu_XY-C5.sh
@@ -0,0 +1,13 @@
+make image PROFILE=xiaoyu_xy-c5 PACKAGES="\
+vim ip-full tc \
+iptables-mod-ipopt iptables-mod-ipsec \
+kmod-sit kmod-tun \
+kmod-ipsec4 kmod-ipsec6 \
+kmod-usb-storage-uas \
+kmod-fs-autofs4 kmod-fs-ext4 \
+kmod-sched"
+
+#kmod-nft-core kmod-nft-nat \
+#kmod-nfnetlink"
+#kmod-nf-nathelper 
+#firewall: kmod-ipt-nat kmod-ipt-conntrack 
